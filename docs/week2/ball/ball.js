cf1231a8bf79d72039f0507ade0830ef449058fe
Nadia.Kramer <nadia.kramer@students.fhnw.ch>
2019-02-26 23:15:49 +0100

    bouncing ball

--- docs/week2/ball/ball.js
+++ docs/week2/ball/ball.js
@@ -6,36 +6,55 @@ let   old  = {x: ball.x, y: ball.y};
 function start() {
     const canvas  = document.getElementById("canvas");
     const context = canvas.getContext("2d");
-    context.fillStyle = "black";
+    context.fillStyle = "white";
 
     setInterval(() => {
         nextBoard();
         display(context);
-    }, 1000 / 20);
+    }, 1000 / 40);
 }
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
+    old.y = old.y + ball.dy;
+    old.x = old.x + ball.dx;
 
     // handle ball is hitting the bounds
     //   reverse direction
     //   lose some energy relative to the current inertia (only velocity varies
+    if (ball.x > 400){
+        ball.dx = -(ball.dx);
+    }
+
+    if (ball.x < 0){
+        ball.dx = -(ball.dx);
+    }
+
+    if (ball.y > 400){
+        ball.dy = -(ball.dy * 0.9);
+    }
+
+    if (ball.y < 0){
+        ball.dy = -(ball.dy * 0.9);
+    }
 
     // calculate new position
     // calculate any changes in velocity due to gravitationial pull or medium resistance
-
-
+    ball.dy += 0.2;
+    ball.x = ball.x + ball.dx;
+    ball.y = ball.y + ball.dy;
 }
 
 function display(context) {
-    context.clearRect(old.x - radius - 1 , old.y - radius -1 , 22, 22 );
-    fillBox(context)
+    context.clearRect(old.x - radius - 5, old.y - radius - 2, 30, 30 );
+    fillBox(context);
 }
 
 function fillBox(context) {
     context.beginPath();
     context.arc(ball.x, ball.y, radius, 0, 6.3, false);
     context.fill();
+
 }
 
 
